276eff64fd996fe9ff39b7b5eca818c255533ae3
Mathieu Dutour <mat.dutour@gmail.com>
2017-09-24 19:45:58 +0200

    add unsubscribe endpoint

---
+++ api/unsubscribe.js
@@ -0,0 +1,143 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET)
+const storage = require('../storage')()
+const cleanBody = require('./_cleanBody')
+
+function refoundCharges (charges, amount) {
+  if (amount <= 0 || !charges.length) {
+    return Promise.resolve()
+  }
+  const chargeToRefound = charges.shift()
+
+  const refoundAmountForCharge = Math.min(amount, chargeToRefound.amount)
+
+  return stripe.refunds.create({
+    charge: chargeToRefound.id,
+    amount: refoundAmountForCharge
+  }).then(
+    () => refoundCharges(charges, amount - refoundAmountForCharge)
+  )
+}
+
+function cancelSubscriptions (subscriptions, refound) {
+  const options = refound ? undefined : { at_period_end: true }
+  return Promise.all(subscriptions.map(
+    subscription => stripe.subscriptions.del(subscription.id, options)
+  ))
+}
+
+module.exports.handler = (event, context, callback) => {
+  let parsedBody
+  try {
+    parsedBody = JSON.parse(event.body || {})
+  } catch (e) {
+    callback(new Error('[400] Could not parse the body'))
+    return
+  }
+
+  const body = cleanBody(parsedBody)
+  if (!body.githubId) {
+    callback(new Error('[400] Missing github ID'))
+    return
+  }
+
+  const refound = !!parsedBody.refound
+  let bailout = false
+  const prorationDate = Math.floor(Date.now() / 1000)
+
+  storage
+    .findOne(body.githubId)
+    .then(found => {
+      if (!found || !found.stripeId) {
+        bailout = true
+        return
+      }
+      body.stripeId = found.stripeId
+    })
+    .then(() => {
+      if (bailout) { return }
+      // find the existing subscription
+      return Promise.all([
+        stripe.subscriptions.list({
+          customer: body.stripeId,
+          limit: 100
+        }),
+        stripe.charges.list({customer: body.stripeId,
+          limit: 100
+        })
+      ])
+    })
+    .then(([subscriptions, charges]) => {
+      if (bailout) { return }
+      if (!refound) {
+        return cancelSubscriptions(subscriptions.data, refound)
+      }
+      return Promise.all(
+        subscriptions.data.map(subscription => {
+          return stripe.invoices.retrieveUpcoming(
+            body.stripeId,
+            subscription.id,
+            {
+              subscription_items: [{
+                id: subscription.items.data[0].id,
+                plan: subscription.items.data[0].plan.id,
+                quantity: 0
+              }],
+              subscription_prorate: true,
+              subscription_proration_date: prorationDate
+            }
+          )
+        })
+      ).then(refoundAmounts => {
+        const totalRefoundAmount = refoundAmounts.reduce((prev, a) => prev + a, 0)
+
+        return refoundCharges(charges.data, totalRefoundAmount)
+      }).then(() => {
+        return cancelSubscriptions(subscriptions.data, refound)
+      })
+    })
+    .then(res => {
+      if (bailout) { return }
+      callback(null, {
+        ok: true,
+        message: 'Subscription canceled'
+      })
+    })
+    .catch((err) => {
+      console.error(err)
+      if (!err.type) {
+        callback(new Error(`[500] ${err}`))
+        return
+      }
+      switch (err.type) {
+        case 'StripeCardError':
+          // A declined card error
+          const message = err.message // => e.g. "Your card's expiration year is invalid."
+          callback(new Error(`[400] ${message}`))
+          break
+        case 'RateLimitError':
+          // Too many requests made to the API too quickly
+          callback(new Error(`[503] Server is a bit overloaded, try again in a bit`))
+          break
+        case 'StripeInvalidRequestError':
+          // Invalid parameters were supplied to Stripe's API
+          callback(new Error(`[400] Bad request`))
+          break
+        case 'StripeAPIError':
+          // An error occurred internally with Stripe's API
+          callback(new Error(`[500] Stripe failed, sorry about that`))
+          break
+        case 'StripeConnectionError':
+          // Some kind of error occurred during the HTTPS communication
+          callback(new Error(`[500] Stripe is down, sorry about that`))
+          break
+        case 'StripeAuthenticationError':
+          // You probably used an incorrect API key
+          callback(new Error(`[500] How did that happen!?`))
+          break
+        default:
+          // Handle any other types of unexpected errors
+          callback(new Error(`[500] ${err.message}`))
+          break
+      }
+    })
+}
