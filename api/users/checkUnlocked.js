ed3c1f868bbcdf165b7d10efb10996b46a052d9c
mathieudutour <mathieu@dutour.me>
2018-08-27 10:54:18 -0400

    return it the valid comes from a enterprise

--- api/users/checkUnlocked.js
+++ api/users/checkUnlocked.js
@@ -26,23 +26,28 @@ module.exports.handler = (event, context, callback) => {
       return storage.create(body).then(() => body)
     })
     .then((user) => {
-      if (!user.valid && !user.validEnterprise && user.orgs && user.orgs.length) {
+      if (user.orgs && user.orgs.length) {
         // check if a user if part of a valid org
         return storage.findOrgs(user.orgs).then(orgs => {
-          user.validEnterprise = orgs.some(org => org.validEnterprise)
-          user.valid = !user.validEnterprise && orgs.some(org => org.valid)
-        }).then(() => {
-          makeCallback(callback, {
-            ok: true,
-            user: user
-          })
-        })
-      } else {
-        makeCallback(callback, {
-          ok: true,
-          user: user
+          if (!user.validEnterprise) {
+            const org = orgs.find(org => org.validEnterprise)
+            user.validEnterprise = !!org
+            user.validEnterpriseFromOrg = !!org
+            user.validEnterpriseFromOrgAdmin = org.admins.some(id => user.githubId === id)
+          }
+          if (!user.valid) {
+            const org = orgs.find(org => org.valid)
+            user.valid = !!org
+            user.validFromOrg = !!org
+            user.validFromOrgAdmin = org.admins.some(id => user.githubId === id)
+          }
+          return user
         })
       }
-    })
+      return user
+    }).then(user => makeCallback(callback, {
+      ok: true,
+      user: user
+    }))
     .catch(callback)
 }
